e2834315cdb87d154117bd07e158eae7e4d9dab3
nimscodes <princenimako28@gmail.com>
2023-04-03 19:40:25 +0100

    client side fetching

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import NavBar from '@/components/navbar/NavBar';
 import PostList from '../components/post_list/PostList';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Categories from './category/Categories';
 import { getAllCategories } from '../../utils/getAllCategories';
 
-export default function Home({posts, categories}) {
+export default function Home({categories}) {
+
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch(`/api/posts/getPosts`);
+      const data = await response.json()
+      setPosts(data.posts)
+    }
+
+    fetchData();
+  }, [])
+  
   return (
       <main className='w-[90%] mx-auto'>
         <NavBar />
@@ -15,15 +27,15 @@ export default function Home({posts, categories}) {
 }
 
 export async function getServerSideProps(context) {
-  let dev = process.env.NODE_ENV !== 'production';
-  let { DEV_URL, PROD_URL } = process.env;
+  // let dev = process.env.NODE_ENV !== 'production';
+  // let { DEV_URL, PROD_URL } = process.env;
   
   try {
-    const res = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts/getPosts`);
-    const { posts } = await res.json();
+    // const res = await fetch(`${dev ? DEV_URL : PROD_URL}/api/posts/getPosts`);
+    // const { posts } = await res.json();
     const categories = await getAllCategories();
 
-    return { props: { posts, categories } };
+    return { props: { categories } };
   } catch (error) {
     console.error(error);
     return { props: {} };
